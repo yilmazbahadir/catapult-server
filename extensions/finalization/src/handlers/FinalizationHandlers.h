67c054f50b838940d1abb7406368a56bb8c22a8e
Jaguar0625 <jaguar0625@127.0.0.1>
2020-06-17 18:52:59 -0400

    add RegisterPushMessagesHandler and related services

---
+++ extensions/finalization/src/handlers/FinalizationHandlers.h
@@ -0,0 +1,33 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#pragma once
+#include "finalization/src/FinalizationMessage.h"
+#include "catapult/handlers/HandlerTypes.h"
+#include "catapult/ionet/PacketHandlers.h"
+
+namespace catapult { namespace handlers {
+
+	/// Prototype for a function that processes a range of finalization messages.
+	using MessageRangeHandler = RangeHandler<finalization::FinalizationMessage>;
+
+	/// Registers a push messages handler in \a handlers that forwards messages to \a messageRangeHandler.
+	void RegisterPushMessagesHandler(ionet::ServerPacketHandlers& handlers, const MessageRangeHandler& messageRangeHandler);
+}}
